42a0f27b57e4c671f695a8030977101fc193d584
JRC-Tech <javier.ruzzante@gmail.com>
2020-10-13 00:04:41 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -51,28 +51,3 @@ wsServer.on('request', function(request) {
     }); */
 });
 
-
-
-
-
-
-
-
-/* const net = require('net');
-//Create a server object:
-const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        console.log(data.toString());
-    });
-
-    socket.write('Hello! this is server speaking...\n');
-    socket.end('Closing connection now...\n');
-}).on('error', (err) => {
-    console.error(err);
-});
-
-//Open server on port 3000:
-server.listen(3000, () => {
-    console.log('Server on port: ', server.address().port);
-});
- */
